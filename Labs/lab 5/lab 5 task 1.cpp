d2ae1e3316d4928a15f55d04abffd75f006adff3
Siouxiezz <fou4chik@gmail.com>
2026-03-24 20:01:00 +0200

    Start of the project

---
+++ Labs/lab 5/lab 5 task 1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct LinkedList
+{
+    int m;
+    LinkedList *next;
+} *first = nullptr, *last = nullptr;
+
+LinkedList *addFirstEl(LinkedList *first, int data)
+{
+    LinkedList *c = new LinkedList;
+    c -> m = data;
+    c -> next = first;
+    first = c;
+    if (last == 0)
+    {
+        last = first;
+        return first;
+    }
+}
+
+LinkedList *addLastEl(LinkedList *first, int data) {
+    LinkedList *c = new LinkedList;
+    c->m = data;
+    c->next = 0;
+    if (first == 0) 
+    {
+        first = c;
+        last = c;
+        return first;
+    }
+    
+    last->next = c;
+    last = c;
+    return first;
+}
+
+int main()
+{
+
+
+    return 0;
+}
