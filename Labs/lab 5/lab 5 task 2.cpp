807ee783bdae7cef1f3db2d861bd0294dc019c63
Siouxiezz <fou4chik@gmail.com>
2026-03-24 23:37:30 +0200

    Some functions

--- Labs/lab 5/lab 5 task 2.cpp
+++ Labs/lab 5/lab 5 task 2.cpp
@@ -30,6 +30,75 @@ City *addCity(City *first, string name, int dist)
     return first;
 }
 
+void showList(City *first) 
+{
+    City *q = first;
+
+    if (q == 0) 
+    {
+        cout << "List is empty\n";
+        return;
+    }
+
+    while (q != 0) 
+    {
+
+        cout << q->name << " " 
+             << q->distance << "km ";
+        q = q->next;
+    }
+    cout << endl;
+}
+
+void printFarthestCities(City *first)
+{
+    City *q = first;
+    City *max1 = 0, *max2 = 0;
+
+    if (first == 0)
+    {
+        cout << "List is empty\n";
+        return;
+    }
+
+    while (q != 0)
+    {
+        if (max1 == 0 || q->distance > max1->distance)
+        {
+            max2 = max1;
+            max1 = q;
+        }
+        else if (max2 == 0 || q->distance > max2->distance)
+        {
+            max2 = q;
+        }
+
+        q = q->next;
+    }
+
+    cout << "Farthest cities:\n";
+
+    if (max1 != 0)
+        cout << max1->name << " " << max1->distance << "km\n";
+
+    if (max2 != 0)
+        cout << max2->name << " " << max2->distance << "km\n";
+        
+}
+
+void deleteList(City *first) 
+{
+    if (first == 0) 
+        return;
+
+    while (first != 0) 
+    {
+        City *temp = first;
+        first = first->next;
+        delete temp;
+    }
+}
+
 int main()
 {
 
